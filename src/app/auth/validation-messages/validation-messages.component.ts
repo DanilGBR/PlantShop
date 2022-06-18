cde8b8bf1a59994a0b685e6a7aa9e7f3efd054b3
GabrielD <daniliuc.gabriel@gmail.com>
2022-06-18 17:44:54 +0300

    validation message update

--- src/app/auth/validation-messages/validation-messages.component.ts
+++ src/app/auth/validation-messages/validation-messages.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
+import error_messages, { ValidationMessagesType } from './validation-messages';
 
 @Component({
   selector: 'app-validation-messages',
@@ -8,61 +9,14 @@ import { FormGroup } from '@angular/forms';
 })
 export class ValidationMessagesComponent implements OnInit {
   public message: string = '';
+  public validationMessages: ValidationMessagesType = error_messages;
 
-  error_messages: Object = {
-    fullName: [
-      { type: 'required', message: 'Full name is required' },
-      {
-        type: 'minlength',
-        message: 'Full name must be at least 5 characters long',
-      },
-      {
-        type: 'maxlength',
-        message: 'Full name cannot be more than 25 characters long',
-      },
-      {
-        type: 'pattern',
-        message: 'Your full name must contain only letters',
-      },
-      {
-        type: 'validUsername',
-        message: 'Your full name has already been taken',
-      },
-    ],
-    email: [
-      { type: 'required', message: 'Email is required' },
-      { type: 'pattern', message: 'Enter a valid email' },
-    ],
-    confirm_password: [
-      { type: 'required', message: 'Confirm password is required' },
-      { type: 'areEqual', message: 'Password mismatch' },
-    ],
-    password: [
-      { type: 'required', message: 'Password is required' },
-      {
-        type: 'minlength',
-        message: 'Password must be at least 5 characters long',
-      },
-      {
-        type: 'pattern',
-        message:
-          'Your password must contain at least one uppercase, one lowercase, and one number',
-      },
-    ],
-    terms: [
-      { type: 'pattern', message: 'You must accept terms and conditions' },
-    ],
-  };
-
-  @Input() form!: FormGroup;
+  @Input() control!: AbstractControl;
+  @Input() controlName: string = '';
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  getErrorMessage(formName: string) {
-    const fullName = this.form.get('fullName');
-    const email = this.form.get('email');
-    const password = this.form.get('password');
-  }
+  testErrorMessage() {}
 }
