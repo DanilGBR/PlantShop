32c225227b2653059d9f59fd50e40c127179df4a
GabrielD <daniliuc.gabriel@gmail.com>
2022-08-28 15:21:25 +0300

    contact component creation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,14 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AcceptModalComponent } from './modals/accept-modal/accept-modal.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 const components = [
   HeaderComponent,
   FooterComponent,
   PageNotFoundComponent,
   AcceptModalComponent,
+  ContactUsComponent,
 ];
 @NgModule({
   declarations: components,
